519f673ff0d6634871ab60969b3bac53c64a469f
OGarmonenko <olga_bodak@list.ru>
2022-09-15 15:29:40 +0300

    Initial commit - setting webpack

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,37 @@
+import type {RuleSetRule} from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import type {BuildOptions} from "./types/config";
+import {types} from "sass";
+import Boolean = types.Boolean;
+
+export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
+    const cssLoader =   {
+            test: /\.s[ac]ss$/i,
+            use: [
+                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            auto: (resPath: string) => !!(resPath.includes('.module.')),
+                            localIdentName: isDev
+                                ? '[path][name]__[local]--[hash:base64:5]'
+                                : '[hash:base64:8]'
+                        },
+                    },
+                },
+                "sass-loader",
+            ],
+        };
+
+    const typeScriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typeScriptLoader,
+        cssLoader
+    ]
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const App = () => {
+    return (
+        <div className="app">
+          Hello
+        </div>
+    );
+};
+
+export default App;
