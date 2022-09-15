3c7de4a569069c5754349806a16a468e726cf250
OGarmonenko <olga_bodak@list.ru>
2022-09-15 19:09:24 +0300

    Developed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './index.scss';
+import {Link, Route, Routes} from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
 
 const App = () => {
     return (
         <div className="app">
-          Hello
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+          <Routes>
+              <Route path={'/about'} element={<AboutPage />} />
+              <Route path={'/'}  element={<MainPage />} />
+          </Routes>
         </div>
     );
 };
