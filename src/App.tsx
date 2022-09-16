0dd98239cfb08983be12ee1bd32a460e6dc07e88
OGarmonenko <olga_bodak@list.ru>
2022-09-16 15:40:02 +0300

    Added theme context and provider, developed hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React, { Suspense } from 'react';
-import './index.scss';
+import React, {Suspense} from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from 'react-router-dom';
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to='/'>Main</Link>
+            <Link to='/about'>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageLazy />} />
-                    <Route path={'/'}  element={<MainPageLazy />} />
+                    <Route path='/about' element={<AboutPageLazy />} />
+                    <Route path='/'  element={<MainPageLazy />} />
                 </Routes>
             </Suspense>
         </div>
