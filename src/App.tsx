bfb2e75cb938018bf74ad2699495a02d33cd46ef
OGarmonenko <olga_bodak@list.ru>
2022-09-16 16:14:24 +0300

    Developed classNames util

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import {Link, Route, Routes} from 'react-router-dom';
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./utils/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to='/'>Main</Link>
             <Link to='/about'>About</Link>
---
+++ src/utils/classNames.ts
@@ -0,0 +1,10 @@
+
+type TMods = Record<string, string|boolean>
+
+export function classNames(cls: string, mods: TMods, additional: string[]):string {
+    return [
+        cls,
+        ...additional,
+        ...Object.keys(mods).filter(key => mods[key])
+    ].join(' ');
+}
