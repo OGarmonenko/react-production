2b84e527bfcbe937317c14c8a5e3d005f40d9400
OGarmonenko <olga_bodak@list.ru>
2022-09-19 10:11:27 +0300

    10 - Start of development fsd architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
-import './styles/index.scss';
+import '@/app/styles/index.scss';
 import {Link, Route, Routes} from 'react-router-dom';
-import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
-import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./utils/classNames";
+import {useTheme} from "@/app/providers/ThemeProvider";
+import {classNames} from '@/shared/lib/classNames/classNames';
+import {AboutPage} from '@/pages/AboutPage';
+import { MainPage } from '@/pages/MainPage';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -16,8 +16,8 @@ const App = () => {
             <Link to='/about'>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path='/about' element={<AboutPageLazy />} />
-                    <Route path='/'  element={<MainPageLazy />} />
+                    <Route path='/about' element={<AboutPage />} />
+                    <Route path='/'  element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
