d775693226c0ed418496b364fdc457b76dd5d5c7
OGarmonenko <olga_bodak@list.ru>
2022-09-20 18:30:18 +0300

    13 - Developed ThemeSwitcher.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,16 @@ import {useTheme} from "@/app/providers/ThemeProvider";
 import {classNames} from '@/shared/lib/classNames/classNames';
 import {AppRouter} from "@/app/providers/router";
 import {Navbar} from "@/widgets/Navbar";
+import {ThemeSwitcher} from "@/shared/ui/ThemeSwitcher/ui/ThemeSwitcher";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
             <AppRouter />
-            <button onClick={toggleTheme}>TOGGLE</button>
+            <ThemeSwitcher />
         </div>
     );
 };
