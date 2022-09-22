930dda7974c9d7a4ed9dcfb58ddf0243c0eb2a08
OGarmonenko <olga_bodak@list.ru>
2022-09-22 11:31:27 +0300

    16 - Added react-refresh-webpack-plugin and prop hot for DevServer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import  webpack, {WebpackPluginInstance} from "webpack";
 import type {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 export function buildPlugins({paths, isDev}: BuildOptions) : WebpackPluginInstance[] {
     return  [
@@ -15,6 +16,7 @@ export function buildPlugins({paths, isDev}: BuildOptions) : WebpackPluginInstan
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        isDev && new ReactRefreshWebpackPlugin(),
     ]
 }
