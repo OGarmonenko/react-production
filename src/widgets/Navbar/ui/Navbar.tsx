51cb0e584783c3ee01a450d9233e7ae81cf1086d
OGarmonenko <olga_bodak@list.ru>
2022-09-22 11:14:32 +0300

    15 - Added i18n to Applink

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,22 @@
 import {classNames} from "@/shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import {AppLink, AppLinkTheme} from "@/shared/ui/Applink/AppLink";
+import {useTranslation} from "react-i18next";
 
 interface INavbarProps {
     className?: string;
 }
 
 export const Navbar = ({className}: INavbarProps) => {
+    const {t} = useTranslation();
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
             <div className={classNames(cls.links)}>
                 <AppLink to='/' theme={AppLinkTheme.INVERTABLE}>
-                    Main
+                    {t('Link Main page')}
                 </AppLink>
                 <AppLink to='/about' theme={AppLinkTheme.INVERTABLE}>
-                    About
+                    {t('Link About us')}
                 </AppLink>
             </div>
         </div>
