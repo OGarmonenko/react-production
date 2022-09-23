21f9a94bb1e5972f7bdf9c147e69940f348bcd9b
OGarmonenko <olga_bodak@list.ru>
2022-09-23 14:06:47 +0300

    21 - Added NotFoundPage

--- src/shared/config/RouteConfig/routeConfig.tsx
+++ src/shared/config/RouteConfig/routeConfig.tsx
@@ -1,13 +1,16 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from '@/pages/MainPage';
 import { AboutPage } from '@/pages/AboutPage';
+import { NotFoundPage } from '@/pages/NotFoundPage';
 
 export const enum AppRoutes {
     MAIN = '/',
     ABOUT = '/about',
+    NOT_FOUND = '*',
 }
 
 export const routeConfig: RouteProps[] = [
     { path: AppRoutes.MAIN, element: <MainPage /> },
     { path: AppRoutes.ABOUT, element: <AboutPage /> },
+    { path: AppRoutes.NOT_FOUND, element: <NotFoundPage /> },
 ];
