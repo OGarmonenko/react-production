14c42608f6a560280a97c39e10df1205b7dc83ae
OGarmonenko <olga_bodak@list.ru>
2022-09-29 15:24:17 +0300

    27 - changed buildCssLoader.ts and buildPlugins.ts

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -12,8 +12,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions) : WebpackPluginInst
         }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].scss',
-            chunkFilename: 'css/[name].[contenthash:8].scss',
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
