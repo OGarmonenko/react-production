6cb4659d1d7e06d139435c0f6fc0a7d029648b98
OGarmonenko <olga_bodak@list.ru>
2022-09-29 13:56:10 +0300

    27 - fixed toggle button

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Sidebar.module.scss';
 import { ThemeSwitcher } from '@/widgets/toSideBar/ThemeSwitcher';
@@ -11,6 +12,7 @@ interface ISidebarProps {
 
 export const Sidebar = ({ className }: ISidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
@@ -24,7 +26,7 @@ export const Sidebar = ({ className }: ISidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                toggle
+                {t('Toggle')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
