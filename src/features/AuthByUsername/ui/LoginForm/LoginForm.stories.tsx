0f4176d4b7bb992b2e646feaaabc3b2c5f9975a9
OGarmonenko <olga_bodak@list.ru>
2022-10-18 16:13:09 +0300

    34 - Fixed LoginForm.stories.tsx

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { Theme } from '@/app/providers/ThemeProvider';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import '@/app/styles/variables/global.scss';
-import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator';
-import { LoginForm } from './LoginForm';
+import LoginForm from './LoginForm';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator';
 
 export default {
