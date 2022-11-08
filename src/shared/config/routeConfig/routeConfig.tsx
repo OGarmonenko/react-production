bb3dd6ca2c7beb0fa93b6d8d5eee308831d715ee
OGarmonenko <olga_bodak@list.ru>
2022-11-09 00:13:00 +0300

    48 - Added articles page and article details page

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -3,6 +3,8 @@ import { MainPage } from '@/pages/MainPage';
 import { AboutPage } from '@/pages/AboutPage';
 import { NotFoundPage } from '@/pages/NotFoundPage';
 import { ProfilePage } from '@/pages/ProfilePage';
+import { ArticlesPage } from '@/pages/ArticlesPage';
+import { ArticleDetailsPage } from '@/pages/ArticleDetailsPage';
 
 export type AppRoutesProps = RouteProps & {
     authOnly?: boolean;
@@ -12,6 +14,8 @@ export const enum AppRoutes {
     MAIN = '/',
     ABOUT = '/about',
     PROFILE = '/profile',
+    ARTICLES = '/articles',
+    ARTICLE_DETAILS = '/articles/', // +id
     NOT_FOUND = '*',
 }
 
@@ -19,5 +23,7 @@ export const routeConfig: AppRoutesProps[] = [
     { path: AppRoutes.MAIN, element: <MainPage /> },
     { path: AppRoutes.ABOUT, element: <AboutPage /> },
     { path: AppRoutes.PROFILE, element: <ProfilePage />, authOnly: true },
+    { path: AppRoutes.ARTICLES, element: <ArticlesPage />, authOnly: true },
+    { path: `${AppRoutes.ARTICLE_DETAILS}:id`, element: <ArticleDetailsPage />, authOnly: true },
     { path: AppRoutes.NOT_FOUND, element: <NotFoundPage /> },
 ];
