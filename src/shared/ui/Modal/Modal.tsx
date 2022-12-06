7f0f232adde902bdf2c0b9e0b37319d633acd838
OGarmonenko <olga_bodak@list.ru>
2022-12-06 11:59:37 +0300

    80 - developed drawer and overlay, added react-device-detect

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -6,6 +6,7 @@ import { classNames, TMods } from '@/shared/lib/classNames/classNames';
 import cls from './Modal.module.scss';
 import { Portal } from '../Portal/Portal';
 import { useTheme } from '@/app/providers/ThemeProvider';
+import { Overlay } from '../Overlay/Overlay';
 
 interface IModalProps {
     className?: string;
@@ -68,10 +69,6 @@ export const Modal = (props: IModalProps) => {
         };
     }, [isOpen, onKeyDown]);
 
-    const onContentClick = (e: React.MouseEvent) => {
-        e.stopPropagation();
-    };
-
     if (lazy && !isMounted) {
         return null;
     }
@@ -79,13 +76,11 @@ export const Modal = (props: IModalProps) => {
     return (
         <Portal>
             <div className={classNames(cls.modal, mods, [className, theme, 'app_modal'])}>
-                <div className={cls.overlay} onClick={closeHandler}>
-                    <div
-                        className={cls.content}
-                        onClick={onContentClick}
-                    >
-                        {children}
-                    </div>
+                <Overlay onClick={closeHandler} />
+                <div
+                    className={cls.content}
+                >
+                    {children}
                 </div>
             </div>
         </Portal>
