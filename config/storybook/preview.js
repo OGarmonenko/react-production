1218600b4ce152975f18f4ca78bfe33c1e64d15b
OGarmonenko <olga_bodak@list.ru>
2023-02-17 21:08:46 +0300

    add theme addon for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,8 +1,10 @@
 import { addDecorator } from '@storybook/react';
-import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
-import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
+
+import { Theme } from '../../src';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,6 +13,14 @@ export const parameters = {
             color: /(background|color)$/i,
             date: /Date$/,
         },
+    },
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+            { name: 'dark', class: Theme.DARK, color: '#000000' },
+            { name: 'blue', class: Theme.BLUE, color: '#5e69ee' },
+        ],
     },
 };
 
